c31a50f91ae40367eb2cd12fa051c203d35b42f4
Juergen Leopold <leopoldj@de.ibm.com>
2016-06-15 14:22:51 +0200

    Added Vagrant test environment.

---
+++ provision.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Update Ubuntu ..."
+apt-get install software-properties-common
+apt-add-repository ppa:ansible/ansible -y
+apt-get update >/dev/null 2>&1
+apt-get -y upgrade >/dev/null 2>&1
+
+echo "Install pip ..."
+apt-get install -y python-pip
+apt-get install -y build-essential
+apt-get install -y python-dev
+
+echo "Install virtualenv ..."
+pip install virtualenv
