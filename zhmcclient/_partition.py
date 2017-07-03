1e384edecd50c3ac3b742bad6c8d9a51ca36ab46
Andreas Maier <maiera@de.ibm.com>
2017-07-03 12:51:29 +0200

    Fixed JSON serialization issue in increase/decrease_crypto_config()
    Details:
    
    - The increase_crypto_config() decrease_crypto_config() methods of
      the Partition class constructed the HTTP request body using a
      generator expression, which cannot be serialized by the json
      module. Fixed that by changing to use a list comprehension,
      instead.
    
    Signed-off-by: Andreas Maier <maiera@de.ibm.com>

--- zhmcclient/_partition.py
+++ zhmcclient/_partition.py
@@ -886,8 +886,7 @@ class Partition(BaseResource):
           :exc:`~zhmcclient.AuthError`
           :exc:`~zhmcclient.ConnectionError`
         """
-        crypto_adapter_uris = (a.uri for a in crypto_adapters)
-
+        crypto_adapter_uris = [a.uri for a in crypto_adapters]
         body = {'crypto-adapter-uris': crypto_adapter_uris,
                 'crypto-domain-configurations': crypto_domain_configurations}
         self.manager.session.post(
@@ -932,8 +931,7 @@ class Partition(BaseResource):
           :exc:`~zhmcclient.AuthError`
           :exc:`~zhmcclient.ConnectionError`
         """
-        crypto_adapter_uris = (a.uri for a in crypto_adapters)
-
+        crypto_adapter_uris = [a.uri for a in crypto_adapters]
         body = {'crypto-adapter-uris': crypto_adapter_uris,
                 'crypto-domain-indexes': crypto_domain_indexes}
         self.manager.session.post(
