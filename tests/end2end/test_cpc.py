7cf4e9f34a5e927561172f3fcc2f2d7c94f28084
Andreas Maier <maiera@de.ibm.com>
2021-12-24 10:59:06 +0100

    End2end tests: Improved CPC find/list tests
    Details:
    
    * Improved the thoroughness of the find/list end2end tests for CPCs.
    
    * Corrected comment in end2end test function for activation profiles.
    
    * Added pytest -v option to make invocation of end2end tests.
    
    Signed-off-by: Andreas Maier <maiera@de.ibm.com>

--- tests/end2end/test_cpc.py
+++ tests/end2end/test_cpc.py
@@ -29,193 +29,205 @@ from zhmcclient.testutils.cpc_fixtures import all_cpcs  # noqa: F401
 
 urllib3.disable_warnings()
 
-# Detect machine generation from machine type
-MACHINE_GENERATIONS = {
-    '2817': 'gen11',  # z196
-    '2818': 'gen11',  # z114
-    '2827': 'gen12',  # EC12
-    '2828': 'gen12',  # BC12
-    '2964': 'gen13',  # z13
-    '2965': 'gen13',  # z13s
-    '3906': 'gen14',  # z14
-    '3907': 'gen14',  # z14-ZR1 (MR)
-}
+# Properties in minimalistic Cpc objects (e.g. find_by_name())
+CPC_MINIMAL_PROPS = ['object-uri', 'name']
 
-# Properties in Cpc objects returned by List CPC Objects
-PROPS_CPC_LIST = {
-    'gen11': ['object-uri', 'name', 'status'],
-    'gen12': ['object-uri', 'name', 'status'],
-    'gen13': ['object-uri', 'name', 'status'],
-    'gen14': ['object-uri', 'name', 'status', 'has-unacceptable-status',
-              'dpm-enabled', 'se-version'],
-}
+# Properties in short Cpc objects (e.g. list() without full properties)
+CPC_SHORT_PROPS = [
+    'object-uri', 'name', 'status', 'has-unacceptable-status', 'dpm-enabled',
+    'se-version', 'target-name']
 
-# Properties in minimalistic Cpc objects
-PROPS_CPC_MINIMAL = ['object-uri', 'name']
+# Properties whose values can change between retrievals
+CPC_VOLATILE_PROPS = [
+    'zcpc-ambient-temperature',
+    'zcpc-dew-point',
+    'zcpc-exhaust-temperature',
+    'zcpc-heat-load',
+    'zcpc-heat-load-forced-air',
+    'zcpc-heat-load-water',
+    'zcpc-humidity',
+    'zcpc-maximum-inlet-air-temperature',
+    'zcpc-maximum-inlet-liquid-temperature',
+    'zcpc-minimum-inlet-air-temperature',
+    'ec-mcl-description'  # TODO: Remove once 'last-update' volat. issue fixed
+]
 
 
-def assert_cpc_minimal(cpc, exp_name, exp_prop_names):
+def assert_cpc_props(cpc, exp_props, ignore_values=None, prop_names=None):
     """
-    Check a Cpc object.
+    Check the properties of a Cpc object.
     """
-    act_prop_names = cpc.properties.keys()
+    checked_prop_names = set()
+    for prop_name in exp_props:
 
-    for prop_name in exp_prop_names:
-        assert prop_name in act_prop_names, \
-            "CPC {0}".format(exp_name)
+        if prop_names is not None and prop_name not in prop_names:
+            continue  # Only check properties in prop_names
 
-    assert cpc.properties['name'] == exp_name, \
-        "CPC {0}".format(exp_name)
-    assert cpc.name == exp_name, \
-        "CPC {0}".format(exp_name)
+        assert prop_name in cpc.properties, \
+            "Property '{p}' not found in Cpc object '{c}'". \
+            format(p=prop_name, c=cpc.name)
+
+        if ignore_values is not None and prop_name not in ignore_values:
+            act_value = cpc.properties[prop_name]
+            exp_value = exp_props[prop_name]
+            assert act_value == exp_value, \
+                "Property '{p}' has unexpected value in Cpc object '{c}'". \
+                format(p=prop_name, c=cpc.name)
+
+        checked_prop_names.add(prop_name)
+
+    extra_prop_names = set(cpc.properties.keys()) - checked_prop_names
+
+    assert not extra_prop_names, \
+        "The following properties were unexpectedly present in Cpc object " \
+        "'{c}' : {e}".format(c=cpc.name, e=', '.join(extra_prop_names))
 
 
-# Printing is disabled by default, rename to test_...() to enable it.
-def disabled_test_print_cpcs(all_cpcs):  # noqa: F811
+def get_cpc_props(session, cpc_uri):
+    """
+    Get CPC properties directly using the "Get CPC Properties" operation.
+    """
+    cpc_props = session.get(cpc_uri)
+    return cpc_props
+
+
+def test_cpc_find_list(hmc_session):  # noqa: F811
     # pylint: disable=redefined-outer-name
     """
-    Print some information about the CPCs under test.
-    """
-    for cpc in sorted(all_cpcs, key=lambda obj: obj.name):
-
-        hd = cpc.manager.client.session.hmc_definition
-
-        print("")
-        print("CPC {} on HMC {}:".format(cpc.name, hd.nickname))
-
-        if cpc.dpm_enabled:
-
-            print("  Partitions:")
-            partitions = cpc.partitions.list()
-            for partition in sorted(partitions, key=lambda obj: obj.name):
-                print("    {}".format(partition.name))
-
-            print("  Adapters:")
-            adapters = cpc.adapters.list()
-            for adapter in sorted(adapters, key=lambda obj: obj.name):
-                print("    {}".format(adapter.name))
-
-        else:
-
-            print("  LPARs:")
-            lpars = cpc.lpars.list()
-            for lpar in sorted(lpars, key=lambda obj: obj.name):
-                print("    {}".format(lpar.name))
-
-            print("  Reset profiles:")
-            reset_profiles = cpc.reset_activation_profiles.list()
-            for reset_profile in sorted(reset_profiles,
-                                        key=lambda obj: obj.name):
-                print("    {}".format(reset_profile.name))
-
-            print("  Load profiles:")
-            load_profiles = cpc.load_activation_profiles.list()
-            for load_profile in sorted(load_profiles,
-                                       key=lambda obj: obj.name):
-                print("    {}".format(load_profile.name))
-
-            print("  Image profiles:")
-            image_profiles = cpc.image_activation_profiles.list()
-            for image_profile in sorted(image_profiles,
-                                        key=lambda obj: obj.name):
-                print("    {}".format(image_profile.name))
-
-
-def test_cpc_find_by_name(hmc_session):  # noqa: F811
-    # pylint: disable=redefined-outer-name
-    """
-    Test that all CPCs in the HMC definition can be found using find_by_name().
+    Test find/list methods for CPCs:
+    - find_by_name(name)
+    - find(**filter_args)
+      - name filter (cache/server-side)
+    - findall(**filter_args)
+      - no filter
+      - name filter (cache/server-side)
+      - status filter (client-side)
+    - list(full_properties=False, filter_args=None)
+      - no filter + short
+      - name filter (cache/server-side) + full
+      - name filter (cache/server-side) + short
+      - status filter (client-side) + short
     """
     client = zhmcclient.Client(hmc_session)
     hd = hmc_session.hmc_definition
     for cpc_name in hd.cpcs:
 
-        # The code to be tested
-        cpc = client.cpcs.find_by_name(cpc_name)
+        # The code to be tested: find_by_name(name)
+        found_cpc = client.cpcs.find_by_name(cpc_name)
 
-        assert_cpc_minimal(cpc, cpc_name, PROPS_CPC_MINIMAL)
+        # Get full properties directly, for comparison
+        exp_cpc_props = get_cpc_props(hmc_session, found_cpc.uri)
 
+        cpc_status = exp_cpc_props['status']  # a client-side filter property
 
-def test_cpc_find_with_name(hmc_session):  # noqa: F811
-    # pylint: disable=redefined-outer-name
-    """
-    Test that all CPCs in the HMC definition can be found using find()
-    with the name as a filter argument.
-    """
-    client = zhmcclient.Client(hmc_session)
-    hd = hmc_session.hmc_definition
-    for cpc_name in hd.cpcs:
+        assert_cpc_props(
+            found_cpc, exp_cpc_props, ignore_values=CPC_VOLATILE_PROPS,
+            prop_names=CPC_MINIMAL_PROPS)
 
-        # The code to be tested
+        # The code to be tested: find() with name filter (cache/server-side)
         found_cpc = client.cpcs.find(name=cpc_name)
 
-        assert_cpc_minimal(found_cpc, cpc_name, PROPS_CPC_MINIMAL)
+        assert_cpc_props(
+            found_cpc, exp_cpc_props, ignore_values=CPC_VOLATILE_PROPS,
+            prop_names=CPC_MINIMAL_PROPS)
 
+        # The code to be tested: findall() with no filter
+        found_cpcs = client.cpcs.findall()
 
-def test_cpc_findall_with_name(hmc_session):  # noqa: F811
-    # pylint: disable=redefined-outer-name
-    """
-    Test that all CPCs in the HMC definition can be found using findall()
-    with the name as a filter argument.
-    """
-    client = zhmcclient.Client(hmc_session)
-    hd = hmc_session.hmc_definition
-    for cpc_name in hd.cpcs:
+        assert len(found_cpcs) == len(hd.cpcs)
+        assert cpc_name in [_cpc.name for _cpc in found_cpcs]
+        found_cpc = [_cpc for _cpc in found_cpcs if _cpc.name == cpc_name][0]
+        assert_cpc_props(
+            found_cpc, exp_cpc_props, ignore_values=CPC_VOLATILE_PROPS,
+            prop_names=CPC_SHORT_PROPS)
 
-        # The code to be tested
+        # The code to be tested: findall() with name filter (server-side)
         found_cpcs = client.cpcs.findall(name=cpc_name)
 
         assert len(found_cpcs) == 1
         found_cpc = found_cpcs[0]
-        assert_cpc_minimal(found_cpc, cpc_name, PROPS_CPC_MINIMAL)
+        assert_cpc_props(
+            found_cpc, exp_cpc_props, ignore_values=CPC_VOLATILE_PROPS,
+            prop_names=CPC_MINIMAL_PROPS)
 
+        # The code to be tested: findall() with status filter (client-side)
+        found_cpcs = client.cpcs.findall(status=cpc_status)
 
-def test_cpc_list_with_name(hmc_session):  # noqa: F811
-    # pylint: disable=redefined-outer-name
-    """
-    Test that all CPCs in the HMC definition can be found using list()
-    with the name as a filter argument.
-    """
-    client = zhmcclient.Client(hmc_session)
-    hd = hmc_session.hmc_definition
-    for cpc_name in hd.cpcs:
+        assert len(found_cpcs) >= 1
+        assert cpc_name in [_cpc.name for _cpc in found_cpcs]
+        found_cpc = [_cpc for _cpc in found_cpcs if _cpc.name == cpc_name][0]
+        assert_cpc_props(
+            found_cpc, exp_cpc_props, ignore_values=CPC_VOLATILE_PROPS,
+            prop_names=CPC_SHORT_PROPS)
 
-        # The code to be tested
+        # The code to be tested: list() with no filter and short properties
+        found_cpcs = client.cpcs.list()
+
+        assert len(found_cpcs) == len(hd.cpcs)
+        assert cpc_name in [_cpc.name for _cpc in found_cpcs]
+        found_cpc = [_cpc for _cpc in found_cpcs if _cpc.name == cpc_name][0]
+        assert_cpc_props(
+            found_cpc, exp_cpc_props, ignore_values=CPC_VOLATILE_PROPS,
+            prop_names=CPC_SHORT_PROPS)
+
+        # The code to be tested: list() with name filter and full properties
+        found_cpcs = client.cpcs.list(
+            full_properties=True, filter_args=dict(name=cpc_name))
+
+        assert len(found_cpcs) == 1
+        found_cpc = found_cpcs[0]
+
+        assert_cpc_props(
+            found_cpc, exp_cpc_props, ignore_values=CPC_VOLATILE_PROPS)
+
+        # The code to be tested: list() with name filter and short properties
         found_cpcs = client.cpcs.list(filter_args=dict(name=cpc_name))
 
-        exp_prop_names = []
-        def_machine_type = hd.cpcs[cpc_name].get('machine_type', None)
-        if def_machine_type:
-            gen = MACHINE_GENERATIONS.get(def_machine_type, None)
-            if gen:
-                exp_prop_names = PROPS_CPC_LIST[gen]
-
         assert len(found_cpcs) == 1
         found_cpc = found_cpcs[0]
-        assert_cpc_minimal(found_cpc, cpc_name, exp_prop_names)
+        assert_cpc_props(
+            found_cpc, exp_cpc_props, ignore_values=CPC_VOLATILE_PROPS,
+            prop_names=CPC_SHORT_PROPS)
 
+        # The code to be tested: list() with status filter (client-side) and
+        # short properties
+        found_cpcs = client.cpcs.list(filter_args=dict(status=cpc_status))
 
-def test_cpc_list_with_name_full(hmc_session):  # noqa: F811
-    # pylint: disable=redefined-outer-name
-    """
-    Test that all CPCs in the HMC definition can be found using list()
-    with full properties and the name as a filter argument.
-    """
-    client = zhmcclient.Client(hmc_session)
-    hd = hmc_session.hmc_definition
-    for cpc_name in hd.cpcs:
+        assert len(found_cpcs) >= 1
+        assert cpc_name in [_cpc.name for _cpc in found_cpcs]
+        found_cpc = [_cpc for _cpc in found_cpcs if _cpc.name == cpc_name][0]
+        assert_cpc_props(
+            found_cpc, exp_cpc_props, ignore_values=CPC_VOLATILE_PROPS,
+            prop_names=CPC_SHORT_PROPS)
 
-        # The code to be tested
-        found_cpcs = client.cpcs.list(filter_args=dict(name=cpc_name),
-                                      full_properties=True)
+# Read-only tests:
+# TODO: Test for dpm_enabled property
+# TODO: Test for maximum_active_partitions property
+# TODO: Test for feature_enabled(feature_name)
+# TODO: Test for feature_info()
+# TODO: Test for export_profiles(profile_area, wait_for_completion=True,
+#                                operation_timeout=None)
+# TODO: Test for get_wwpns(partitions)
+# TODO: Test for get_free_crypto_domains(crypto_adapters=None)
+# TODO: Test for get_energy_management_properties()
+# TODO: Test for validate_lun_path(host_wwpn, host_port, wwpn, lun)
+# TODO: Test for export_dpm_configuration()
 
-        exp_prop_names = []
-        def_machine_type = hd.cpcs[cpc_name].get('machine_type', None)
-        if def_machine_type:
-            gen = MACHINE_GENERATIONS.get(def_machine_type, None)
-            if gen:
-                exp_prop_names = PROPS_CPC_LIST[gen]
-
-        assert len(found_cpcs) == 1
-        found_cpc = found_cpcs[0]
-        assert_cpc_minimal(found_cpc, cpc_name, exp_prop_names)
+# Modifying tests:
+# TODO: Test for update_properties(properties)
+# TODO: Test for start(wait_for_completion=True, operation_timeout=None)
+# TODO: Test for stop(wait_for_completion=True, operation_timeout=None)
+# TODO: Test for import_profiles(profile_area, wait_for_completion=True,
+#                                operation_timeout=None)
+# TODO: Test for set_power_save(power_saving, wait_for_completion=True,
+#                               operation_timeout=None)
+# TODO: Test for set_power_capping(power_capping_state, power_cap=None,
+#                                  wait_for_completion=True,
+#                                  operation_timeout=None)
+# TODO: Test for add_temporary_capacity(record_id, software_model=None,
+#                                       processor_info=None, test=False,
+#                                       force=False)
+# TODO: Test for remove_temporary_capacity(record_id, software_model=None,
+#                                          processor_info=None)
+# TODO: Test for set_auto_start_list(auto_start_list)
+# TODO: Test for import_dpm_configuration(dpm_configuration)
