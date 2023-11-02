95f05fb10f1e708ea8f0ec9f7c8457370ffdc437
Andreas Maier <maiera@de.ibm.com>
2023-11-02 22:16:34 +0100

    Release 1.11.4
    Signed-off-by: Andreas Maier <maiera@de.ibm.com>

--- zhmcclient/_version.py
+++ zhmcclient/_version.py
@@ -27,7 +27,7 @@ __all__ = ['__version__']
 #:
 #: * "M.N.P.dev1": A not yet released version M.N.P
 #: * "M.N.P": A released version M.N.P
-__version__ = '1.11.4.dev1'
+__version__ = '1.11.4'
 
 # Check supported Python versions
 # Keep these Python versions in sync with:
