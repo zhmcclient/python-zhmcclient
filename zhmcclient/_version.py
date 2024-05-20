b8006a4e89eedca9ce1cad2ab738f1956247a7d1
Andreas Maier <maiera@de.ibm.com>
2024-05-20 09:59:45 +0200

    Migrated to pyproject.toml
    Details:
    
    * Dev: Migrated from setup.py to pyproject.toml with setuptools as build
      backend. This provides for automatic determination of the package version
      without having to edit a version file.
    
    * In addition to the `zhmcclient.__version__` property which provides the
      package version as a string, a new `zhmcclient.__version_tuple__` property
      provides it as a tuple of integer values.
    
    More internal details (not in change log):
    
    * setuptools-scm generates the versions into a new file
      zhmcclient/_version_scm.py, which gets imported into zhmcclient/_version.py
      to maintain its interface. The new _version_scm.py file is ignored by git.
      and excluded from package_py_files, so it is no longer included in flake8
      or pylint checks (where it would fail).
    
    * Removed inclusion of other requirements/constraints files with '-r' and '-c'
      because the dynamic support for dependencies in setuptools does not
      support these options. This has been replaced by ensuring that the
      "included" files are honored, in the Makefile.
    
    * Renamed minimum-constraints.txt to minimum-constraints-develop.txt
      because it no longer includes minimum-constraints-install.txt
    
    * Renamed .safety-policy-all.yml to .safety-policy-develop.yml to match
      its scope of only covering development dependencies.
    
    * Added a base-requirements.txt file for basic dependents needed for
      pip and automatic package version detection. This replaces the rather
      hard coded installation of pip,setuptools,wheel in the Makefile.
      It also removes the support for first ensuring the minimum version
      of pip to be >=9.0.1 before using requirements files.
    
    * Adjusted the retrieval of the package version in docs/conf.py to the new
      version file created by setuptools-scm. This caused any Sphinx commands
      now to depend on the wheel archive, since that is the step that creates
      the setuptools-scm version file.
    
    * Updated the release description in docs/development.rst to no longer edit
      the version file, but to set tags. A new initial tag is now used on the
      master branch that makes setuptools-scm increase the correct version
      number. For consistency, that approach is also used for stable branches.
    
    Signed-off-by: Andreas Maier <maiera@de.ibm.com>

--- zhmcclient/_version.py
+++ zhmcclient/_version.py
@@ -18,16 +18,27 @@ Definition of the package version, and check for supported Python versions.
 
 import sys
 
-__all__ = ['__version__']
+from ._version_scm import version, version_tuple
+
+__all__ = ['__version__', '__version_tuple__']
 
 #: The full version of this package including any development levels, as a
 #: :term:`string`.
 #:
 #: Possible formats for this version string are:
 #:
-#: * "M.N.P.dev1": A not yet released version M.N.P
+#: * "M.N.Pa1.dev7+g1234567": A not yet released version M.N.P
 #: * "M.N.P": A released version M.N.P
-__version__ = '1.17.0.dev1'
+__version__ = version
+
+#: The full version of this package including any development levels, as a
+#: tuple of version items, converted to integer where possible.
+#:
+#: Possible formats for this version string are:
+#:
+#: * (M, N, P, 'a1', 'dev7', 'g1234567'): A not yet released version M.N.P
+#: * (M, N, P): A released version M.N.P
+__version_tuple__ = version_tuple
 
 # Check supported Python versions
 # Keep these Python versions in sync with:
